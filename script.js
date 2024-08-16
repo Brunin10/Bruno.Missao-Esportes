6805d12a3b1a76c3a10e96cf498e1e18d8548f90
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-08-16 12:37:38 -0300

    rwe

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+        }
+]
