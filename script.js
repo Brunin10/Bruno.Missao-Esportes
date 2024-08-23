114d109c42df8e67d33da7cd44e754cf2aedfeca
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-08-23 12:10:50 -0300

    sffwe

--- script.js
+++ script.js
@@ -8,22 +8,39 @@ const perguntas = [
     {
         enunciado: "Pensando em melhorar o desempenho dos atletas olímpicos para as Olimpíadas de 2028 (Los Angeles), o governo brasileiro deveria investir mais no esporte?",
         alternativas: [
-            "Sim, os atletas precisam de incentivos maiores para melhorar seu desempenho. Além disso, é necessário incentivar o esporte nas escolas para descobrir novos e futuros atletas",
-            "Não, o governo brasileiro deveria utilizar o dinheiro público em outras áreas de maior importância, tais como: saúde e educação",
+            {
+                texto: "Sim, os atletas precisam de incentivos maiores para melhorar seu desempenho. Além disso, é necessário incentivar o esporte nas escolas para descobrir novos e futuros atletas",
+                afirmacao: "afirmacao"
+            };
+            {
+                texto: "Não, o governo brasileiro deveria utilizar o dinheiro público em outras áreas de maior importância, tais como: saúde e educação",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Como o governo deveria distribuir o dinheiro investido no esporte?",
         alternativas: [
-            "Aumentando o valor do bolsa-atleta, construção de novas estruturas esportivas em localidades sem nenhuma estrutura, incentivo e apoio financeiro para novos atletas e futuros treinadores",
-            "Ampliando os centros de treinamento já construídos, investindo na formação dos treinadores já atuantes",
+            {
+                texto: "Aumentando o valor do bolsa-atleta, construção de novas estruturas esportivas em localidades sem nenhuma estrutura, incentivo e apoio financeiro para novos atletas e futuros treinadores",
+                afirmacao: "afirmacao"
+            };
+            {
+                texto: "Ampliando os centros de treinamento já construídos, investindo na formação dos treinadores já atuantes",
+                afirmacao: "afirmacao"
+            }
         ]
     },
-    {
+            {
         enunciado: "Como a educação poderia incentivar o esporte para crianças e adolescentes dentro e fora da escola?",
         alternativas: [
-            "Os profissionais de Educação Física deveriam incentivar a prática esportiva de diversas modalidades, ampliação das aulas de Educação Física, contratação de treinadores para a prática esportiva em período contra-turno",
-            "A educação deveria se preocupar com a formação básica e ética do cidadão, visto que a carreira esportiva são para poucos",
+            {
+                texto: "Os profissionais de Educação Física deveriam incentivar a prática esportiva de diversas modalidades, ampliação das aulas de Educação Física, contratação de treinadores para a prática esportiva em período contra-turno",
+                afirmacao: "afirmacao"
+            }
+            {   texto: "A educação deveria se preocupar com a formação básica e ética do cidadão, visto que a carreira esportiva são para poucos",
+                afirmacao: "afirmacao"
+            }
         ]
     },
 ];
