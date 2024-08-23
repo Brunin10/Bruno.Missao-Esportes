58505e3c715a978caab72f4fdcacd10db26879b1
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-08-23 12:22:48 -0300

    kpokçk

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const perguntas = [
             {
                 texto: "Sim, os atletas precisam de incentivos maiores para melhorar seu desempenho. Além disso, é necessário incentivar o esporte nas escolas para descobrir novos e futuros atletas",
                 afirmacao: "afirmacao"
-            };
+            },
             {
                 texto: "Não, o governo brasileiro deveria utilizar o dinheiro público em outras áreas de maior importância, tais como: saúde e educação",
                 afirmacao: "afirmacao"
@@ -24,7 +24,7 @@ const perguntas = [
             {
                 texto: "Aumentando o valor do bolsa-atleta, construção de novas estruturas esportivas em localidades sem nenhuma estrutura, incentivo e apoio financeiro para novos atletas e futuros treinadores",
                 afirmacao: "afirmacao"
-            };
+            },
             {
                 texto: "Ampliando os centros de treinamento já construídos, investindo na formação dos treinadores já atuantes",
                 afirmacao: "afirmacao"
@@ -37,7 +37,7 @@ const perguntas = [
             {
                 texto: "Os profissionais de Educação Física deveriam incentivar a prática esportiva de diversas modalidades, ampliação das aulas de Educação Física, contratação de treinadores para a prática esportiva em período contra-turno",
                 afirmacao: "afirmacao"
-            };
+            },
             {
                 texto: "A educação deveria se preocupar com a formação básica e ética do cidadão, visto que a carreira esportiva são para poucos",
                 afirmacao: "afirmacao"
