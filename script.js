3867f8acc9b330faef97894c381b2d18d0272dcd
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-08-30 11:31:52 -0300

    lksfaslkdffewrwr

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -59,10 +60,15 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", => respostaSelecionada()){
-
+        botaoAlternativas.addEventListener("click", => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++
+    mostraPergunta();
+}
 mostraPergunta();
